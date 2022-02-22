72bab96c196f95a16c0833047729b1635eb92414
swordfatih <fatih91kilic@gmail.com>
2022-02-22 10:50:50 +0100

    modification

--- BTTP/src/application/Application.java
+++ BTTP/src/application/Application.java
@@ -1,6 +1,5 @@
 package application;
 
-import bttp.ServeurBTTP;
 import client.ClientBTTP;
 
 import java.util.Scanner;
@@ -17,7 +16,6 @@ public class Application {
         System.out.println("Entrez le port du serveur");
         int port = in.nextInt();
 
-        //new Thread(new ServeurBTTP(PORT)).start();
         new Thread(new ClientBTTP(host, port)).start();
     }
 }
